aad2f77c0fa3193995cb3ec6d6c2347c8e15e111
Darien Romero <darien.romero@mediabyte.com.pe>
2024-02-13 12:01:57 -0500

    audio to text

--- src/presentation/components/chat-input-boxes/TextMessageBoxFile.tsx
+++ src/presentation/components/chat-input-boxes/TextMessageBoxFile.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useRef, useState } from "react";
 
 interface Props {
-  onSendMessage: (message: string) => void;
+  onSendMessage: (message: string, file: File) => void;
   placeholder?: string;
   disableCorrections?: boolean;
   accept?: string;//image/*
@@ -22,9 +22,11 @@ export const TextMessageBoxFile = ({
 
   const handleSendMessage = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if(message.trim().length === 0) return;
-    onSendMessage(message);
-    setMessage('')
+    // if(message.trim().length === 0) return;
+    if(!selectedFile) return;
+    onSendMessage(message, selectedFile);
+    setMessage('');
+    setSelectedFile(null);
   }
 
   return (
--- src/presentation/pages/audio-to-text/AudioToTextPage.tsx
+++ src/presentation/pages/audio-to-text/AudioToTextPage.tsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import { useState } from "react"
+import { GptMessage, MyMessage, TypingLoader, TextMessageBoxFile } from "../../components";
+import { audioToTextUseCase } from "../../../core/usecases/audio-to-text.use-case";
+
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+
+}
 
 export const AudioToTextPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string, audioFile: File) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, {text: text, isGpt: false}]);
+    const resp = await audioToTextUseCase(audioFile, text)
+    setIsLoading(false);
+    if(!resp) return;//No hay respuesta
+    const gptMessage = `
+      ##Transcripción:
+      __Duración:__ ${Math.round(resp.duration)}
+      ### El texto fue
+      ${resp.text}
+
+    `
+    setMessages((prev) => [
+      ...prev,
+      {
+        text: gptMessage,
+        isGpt: true
+      }
+    ])
+  }
+
   return (
-    <div>AudioToTextPage</div>
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+          {/* Bienvenido */}
+          <GptMessage text="Hola, puedes escribir subir tu archivo y te muestro las traducciones"/>
+          {
+            messages.map((message, index) => (
+              message.isGpt ? (
+                <GptMessage key={index} text={message.text}/>
+              ) : (
+                <MyMessage key={index} text ={message.text}/>
+              )
+            )) 
+          }
+          {isLoading && <div className="col-start-1 col-end-12 fade-in">
+            <TypingLoader className="fade-in"/>
+          </div>}
+        </div>
+      </div>
+      <TextMessageBoxFile
+        onSendMessage={handlePost}
+        placeholder = 'Escribe aquí lo que deseas'
+        disableCorrections
+        accept="audio/*"
+      />
+    </div>
   )
 }
